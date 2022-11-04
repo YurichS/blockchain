f413241cc1f082bc462fa53929e941311c1f694d
Yurii <yurasidor26@gmail.com>
2022-11-04 15:39:56 +0200

    Big/Little endian to HEX

--- HEX_to_INT.py
+++ HEX_to_INT.py
@@ -2,17 +2,17 @@ def hex_to_little_endian(number):
     result = bytearray.fromhex(number)[::-1]
     return int(bytearray.hex(result), base=16)
 
+
 def hex_to_big_endian(number):
     result = bytearray.fromhex(number)
     return int(bytearray.hex(result), base=16)
 
 
-def little_endian_to_hex(number):
-    pass
+def little_endian_to_hex(number, bytes_amount):
+    return int(number).to_bytes(bytes_amount, "little").hex()
 
 
-def big_endian_to_hex(number):
-    pass
+def big_endian_to_hex(number, bytes_amount):
+    return int(number).to_bytes(bytes_amount, "big").hex()
 
 
-print(hex_to_big_endian('ff00000000000000000000000000000000000000000000000000000000000000'))
