ae4df43645bff99772e35330509f5482f9d30302
Yurii <yurasidor26@gmail.com>
2022-11-04 15:43:48 +0200

    Add decriptions

--- HEX_to_INT.py
+++ HEX_to_INT.py
@@ -2,17 +2,21 @@ def hex_to_little_endian(number):
     result = bytearray.fromhex(number)[::-1]
     return int(bytearray.hex(result), base=16)
 
+#print(hex_to_little_endian("Enter HEX number as a string here"))
 
 def hex_to_big_endian(number):
     result = bytearray.fromhex(number)
     return int(bytearray.hex(result), base=16)
 
+# print(hex_to_big_endian("Enter HEX number as a string here"))
 
 def little_endian_to_hex(number, bytes_amount):
     return int(number).to_bytes(bytes_amount, "little").hex()
 
+# print(little_endian_to_hex("Enter Little endian as a string here", "Enter bytes amount as INT here"))
 
 def big_endian_to_hex(number, bytes_amount):
     return int(number).to_bytes(bytes_amount, "big").hex()
 
+# print(big_endian_to_hex("Enter Little endian as a string here", "Enter bytes amount as INT here"))
 
